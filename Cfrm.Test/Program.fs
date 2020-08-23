957450c393c6401f666f0157cd2c8dcb2f16e624
Brian Berns <brianberns@gmail.com>
2020-08-23 02:51:36 -0400

    Run unit test in exe.

--- Cfrm.Test/Program.fs
+++ Cfrm.Test/Program.fs
@@ -1,7 +1,15 @@
 namespace Cfrm.Test
 
+open System.Diagnostics
+
 module Program =
 
     [<EntryPoint>]
     let main argv =
+        let test = KuhnPokerTest()
+        let stopwatch = Stopwatch()
+        stopwatch.Start()
+        test.Run()
+        stopwatch.Stop()
+        printfn "%A" stopwatch.Elapsed
         0
